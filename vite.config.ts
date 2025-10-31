b50cbb4348440bc78fa883ed280060bf3c5fe18f
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-10-31 16:22:56 +0900

    chore: vite.config 설정

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,16 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite';
+import react from '@vitejs/plugin-react';
+import path from 'path';
 
-// https://vite.dev/config/
+// Vite 설정
 export default defineConfig({
   plugins: [react()],
-})
+  resolve: {
+    alias: {
+      '@': path.resolve(__dirname, './src'), // src를 @로 접근 가능하게
+    },
+  },
+  css: {
+    postcss: './postcss.config.cjs', // PandaCSS용 PostCSS 설정 연결
+  },
+});
