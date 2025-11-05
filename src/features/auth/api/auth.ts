c27f4247ac80cf56b3a17357fbb2f63587f09fae
yeeun <jovm268@naver.com>
2025-11-05 22:45:53 +0900

    refactor : Fetch 기반 API 함수로 변경

--- src/features/auth/api/auth.ts
+++ src/features/auth/api/auth.ts
@@ -1,40 +1,56 @@
 // src/features/auth/api/authApi.ts
 
+import { RequestLoginDTO, RequestSignUpDTO } from '../types/auth';
+
 //MSW와 통신하는 API로직
 //* (fetch 사용 )
-// export default async function signUp(data: RequestSignUpDTO) {
-//   const res = await fetch('/api/auth/signup', {
-//     method: 'POST',
-//     headers: {
-//       'Content-Type': 'application/json',
-//     },
-//     body: JSON.stringify(data),
-//   });
+export async function signUp(data: RequestSignUpDTO) {
+  const res = await fetch('/api/auth/signup', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
 
-//   if (!res.ok) {
-//     const error = await res.json();
-//     throw new Error(error.error || '회원가입 실패');
-//   }
+  if (!res.ok) {
+    throw new Error('회원가입 실패');
+  }
 
-//   return res.json();
-// }
+  return res.json();
+}
+export async function logIn(data: RequestLoginDTO) {
+  const res = await fetch('/api/auth/login', {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+    body: JSON.stringify(data),
+  });
 
-import axios from 'axios';
-import {
-  RequestLoginDTO,
-  RequestSignUpDTO,
-  ResponseLoginDTO,
-  ResponseSignUpDTO,
-} from '../types/auth';
+  if (!res.ok) {
+    throw new Error('로그인실패!');
+  }
+
+  return res.json();
+}
 
 //* Axios사용
+// import axios from 'axios';
+// import {
+//   RequestLoginDTO,
+//   RequestSignUpDTO,
+//   ResponseLoginDTO,
+//   ResponseSignUpDTO,
+// } from '../types/auth';
+
 //body타입은 리퀘스트타입으로 받아옴
 //반환값은 프로미스의 리스폰스타입임
-export const signUp = async (body: RequestSignUpDTO): Promise<ResponseSignUpDTO> => {
-  const { data } = await axios.post('/api/auth/signup', body); //body받아서 넘겨주고 응답값은 data임 무조건
-  return data;
-};
-export const logIn = async (body: RequestLoginDTO): Promise<ResponseLoginDTO> => {
-  const { data } = await axios.post('/api/auth/login', body); //body받아서 넘겨주고 응답값은 data임 무조건
-  return data;
-};
+// export const signUp = async (body: RequestSignUpDTO): Promise<ResponseSignUpDTO> => {
+//   const { data } = await axios.post('/api/auth/signup', body); //body받아서 넘겨주고 응답값은 data임 무조건
+//   return data;
+// };
+// export const logIn = async (body: RequestLoginDTO): Promise<ResponseLoginDTO> => {
+//   const { data } = await axios.post('/api/auth/login', body); //body받아서 넘겨주고 응답값은 data임 무조건
+//   return data;
+// };
