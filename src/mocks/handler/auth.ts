b12d5250c4b6a5bb502b1d4f63d12b7416dd0b4a
yeeun <jovm268@naver.com>
2025-11-05 18:07:46 +0900

    API : MSW 로그인 핸들러 함수 설정

--- src/mocks/handler/auth.ts
+++ src/mocks/handler/auth.ts
@@ -1,10 +1,18 @@
 import { http, HttpResponse } from 'msw';
-import { RequestSignUpDTO } from '../../features/auth/types/auth';
+import {
+  RequestLoginDTO,
+  RequestSignUpDTO,
+  ResponseLoginDTO,
+} from '../../features/auth/types/auth';
 import { mockPasswords, mockUsers } from '../data/user';
 
+//에러타입
+type ErrorResponse = {
+  message: string;
+};
 // src/mocks/handlers/auth.ts
 export const authHandlers = [
-  // 회원가입 API
+  // - 회원가입 API
   http.post('/api/auth/signup', async ({ request }) => {
     const body = (await request.json()) as RequestSignUpDTO;
 
@@ -31,4 +39,40 @@ export const authHandlers = [
 
     return HttpResponse.json({ message: '회원가입 완료' }, { status: 201 });
   }),
+
+  //- 로그인 API
+  http.post<never, RequestLoginDTO, ResponseLoginDTO | ErrorResponse>(
+    '/api/auth/login',
+    async ({ request }) => {
+      const { email } = await request.json(); //사용자의 이메일과 패스워드 받아옴
+
+      //사용자 찾기
+      const user = mockUsers.find((user) => user.email === email);
+
+      //실패! 사용자 음슴
+      if (!user) {
+        return HttpResponse.json(
+          { message: '이메일 또는 비밀번호가 일치하지 않습니다.' },
+          { status: 401 }
+        );
+      }
+
+      //성공~! 토큰과 사용자 정보 반환
+      return HttpResponse.json<ResponseLoginDTO>(
+        {
+          token: 'mock-jwt-token-' + user.id,
+          user: {
+            id: user.id,
+            email: user.email,
+            nickname: user.nickname,
+            name: user.name,
+            phone: user.phone,
+            gender: user.gender,
+            age: user.age,
+          },
+        },
+        { status: 200 }
+      );
+    }
+  ),
 ];
