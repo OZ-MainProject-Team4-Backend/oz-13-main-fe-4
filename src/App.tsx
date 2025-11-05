2db8f6799665370510b2ee953e4fa0fc272eaee7
yeeun <jovm268@naver.com>
2025-11-05 11:18:16 +0900

    style : App인라인 스타일,App.css로이동

--- src/App.tsx
+++ src/App.tsx
@@ -1,21 +1,8 @@
-import { css } from '@emotion/react';
 import './App.css';
+import SignUp from './pages/auth/SignUp';
 
 function App() {
-  return (
-    <div
-      css={css`
-        background-color: #dbeafe;
-        min-height: 100vh;
-        display: flex;
-        direction: column;
-        gap: 20px;
-        justify-content: center;
-        align-items: center;
-        font-family: 'Pretendard', sans-serif;
-      `}
-    />
-  );
+  return <SignUp />;
 }
 
 export default App;
