e5b783bd8d4d76679d7ac44dc50d8c36f4431202
yeeun <jovm268@naver.com>
2025-11-06 13:29:32 +0900

    chore : 로그인 상태 확인하기 위한 버튼 추가

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,12 +1,24 @@
 import { Button } from '@mui/material';
-import { Link } from 'react-router-dom';
+import { Link, useNavigate } from 'react-router-dom';
+import { useAuthStore } from '../features/auth/store/authStore';
 
 export default function Home() {
+  const userAuth = useAuthStore();
+  const navigator = useNavigate();
+  //로그아웃기능(zustand의 스토어 활용)
+  const handleLogout = () => {
+    userAuth.clearAuth();
+    navigator('/');
+  };
   return (
     <div>
       <h1>홈페이지 입니다.</h1>
-      <h3>--님 로그인중</h3>
-      <Button variant='contained' color='secondary'>
+      {userAuth.user?.name ? (
+        <h1>{`${userAuth.user?.name}님 로그인증`}</h1>
+      ) : (
+        <p>로그인 필요합니다.</p>
+      )}
+      <Button variant='contained' color='secondary' onClick={handleLogout}>
         로그아웃하기
       </Button>
       <Link to={'/login'}>로그인하기</Link>
