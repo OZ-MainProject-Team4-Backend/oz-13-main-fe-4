66c18322a52832675c192e4358de332e11d30019
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-11-06 14:29:20 +0900

    chore: DiaryCalendar 스타일 파일 분리

--- src/features/diary/components/DiaryCalendar.tsx
+++ src/features/diary/components/DiaryCalendar.tsx
@@ -1,124 +1,20 @@
-import { css } from '@emotion/react';
 import { IoChevronBack, IoChevronForward } from 'react-icons/io5';
 import { DAYS, MONTHS } from '../../../constants/calenderConst';
 import { useState } from 'react';
 import DiaryModal from './DiaryModal';
-
-const containerStyle = css`
-  margin: 0 auto;
-  min-width: 700px;
-  max-width: 1200px;
-  min-height: 700px;
-  background-color: white;
-  display: flex;
-  flex-direction: column;
-`;
-
-const headerStyle = css`
-  padding: 25px 0;
-  display: flex;
-  justify-content: center;
-  align-items: center;
-  gap: 20px;
-`;
-
-const navButtonStyle = css`
-  background: transparent;
-  border: none;
-  font-size: 24px;
-  color: #666;
-  display: flex;
-  align-items: center;
-  cursor: pointer;
-
-  &:hover {
-    color: #333;
-  }
-`;
-
-const titleStyle = css`
-  font-size: 24px;
-  font-weight: 600;
-  color: #1f1e1e;
-  margin: 0;
-`;
-
-const daysHeaderStyle = css`
-  display: grid;
-  grid-template-columns: repeat(7, 1fr);
-  border: 1px solid #ddd;
-`;
-
-const dayLabelStyle = css`
-  color: #718096;
-  text-align: center;
-  padding: 8px 0;
-  font-weight: 500;
-`;
-
-const calendarBodyStyle = css`
-  display: grid;
-  grid-template-columns: repeat(7, 1fr);
-  flex: 1;
-`;
-
-const dateStyle = css`
-  position: relative;
-  text-align: left;
-  padding: 10px 0 0 10px;
-  border-right: 1px solid #ddd;
-  border-bottom: 1px solid #ddd;
-  min-height: 100px;
-  cursor: pointer;
-`;
-
-const currentMonthDateStyle = css`
-  ${dateStyle}
-  background-color: white;
-`;
-
-const otherMonthDateStyle = css`
-  ${dateStyle}
-  color: #bbb;
-  background-color: #fafafa;
-`;
-
-const todayCircleStyle = css`
-  position: absolute;
-  top: 8px;
-  left: 8px;
-  color: white;
-  background-color: #1976d2;
-  border-radius: 50%;
-  width: 20px;
-  height: 20px;
-  text-align: center;
-  font-weight: 700;
-  padding: 2px;
-`;
-
-const addDiaryButtonStyle = css`
-  position: absolute;
-  top: 40px;
-  left: 50%;
-  transform: translateX(-50%);
-  background-color: var(--color-background);
-  color: #10498a;
-  border: none;
-  border-radius: 16px;
-  padding: 6px 5px;
-  font-size: 13px;
-  font-weight: 500;
-  cursor: pointer;
-  white-space: nowrap;
-  text-align: center;
-  width: 80%;
-
-  &:hover {
-    background-color: #1976d2;
-    color: white;
-  }
-`;
+import {
+  addDiaryButtonStyle,
+  calendarBodyStyle,
+  containerStyle,
+  currentMonthDateStyle,
+  dayLabelStyle,
+  daysHeaderStyle,
+  headerStyle,
+  navButtonStyle,
+  otherMonthDateStyle,
+  titleStyle,
+  todayCircleStyle,
+} from './DiaryCalendar.styles';
 
 const DiaryCalendar = () => {
   const [currentDate, setCurrentDate] = useState(new Date());
