3516eb3b0901e3749f58088498cf66eaa5154d23
yeeun <jovm268@naver.com>
2025-11-06 10:35:58 +0900

    chore : 메인페이지 사용자 정보 확인

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,3 +1,15 @@
+import { Button } from '@mui/material';
+import { Link } from 'react-router-dom';
+
 export default function Home() {
-  return <h1>홈페이지 입니다.</h1>;
+  return (
+    <div>
+      <h1>홈페이지 입니다.</h1>
+      <h3>--님 로그인중</h3>
+      <Button variant='contained' color='secondary'>
+        로그아웃하기
+      </Button>
+      <Link to={'/login'}>로그인하기</Link>
+    </div>
+  );
 }
