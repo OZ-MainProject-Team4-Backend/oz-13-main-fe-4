094ca2ea7ef4966bb5d663fece8f60c443bcd162
yeeun <jovm268@naver.com>
2025-11-06 14:58:12 +0900

    chore : 마이페이지 파일만 추가

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,19 @@
 import { Route, Routes } from 'react-router-dom';
 import './App.css';
-import Home from './pages/Home';
+import DiaryCalendar from './features/diary/components/DiaryCalendar';
 import LogIn from './pages/auth/LogIn';
 import SignUp from './pages/auth/SignUp';
+import { Mainpage } from './pages/main/MainPage';
+import Mypage from './pages/Mypage';
 
 function App() {
   return (
     <Routes>
-      <Route path='/' element={<Home />} />
+      <Route path='/' element={<Mainpage />} />
       <Route path='/login' element={<LogIn />} />
       <Route path='/signup' element={<SignUp />} />
+      <Route path='/mypage' element={<Mypage />} />
+      {/* <Route path='/diary' element={<DiaryCalendar />} /> 추후 업데이트 예정*/}
     </Routes>
   );
 }
