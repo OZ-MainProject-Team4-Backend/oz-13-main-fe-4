8df34131b4e1a854e71df1e6e81193bfff8c6c5a
yeeun <jovm268@naver.com>
2025-11-10 18:14:44 +0900

    feat : 리프레쉬 토큰을 보내서 액세스토큰을 발급받는 기능 구현

--- src/features/auth/api/auth.ts
+++ src/features/auth/api/auth.ts
@@ -6,10 +6,12 @@ import {
   RequestLoginDTO,
   RequestNicknameValidateDTO,
   RequestSignUpDTO,
+  RequestTokenRefreshDTO,
   ResponseEmailSendDTO,
   ResponseEmailVerifyDTO,
   ResponseLoginDTO,
   ResponseNicknameValidateDTO,
+  ResponseTokenRefreshDTO,
 } from '../types/auth';
 
 //- ==================== 닉네임 검증 ====================
@@ -80,6 +82,27 @@ export async function signUp(data: RequestSignUpDTO): Promise<{ message: string 
 
   return res.json();
 }
+//- ==================== 리프레쉬토큰으로 액세스토큰 발급(자동로그인한 유저가 재접속하거나, 자동갱신) ====================
+export async function refreshAccessToken(
+  data?: RequestTokenRefreshDTO
+  //보내는 데이터 , 빈객체 {} 혹은 {refreshToken}
+  //받는 데이터 , { accessToken } (새로 발급된 액세스 토큰만)
+): Promise<ResponseTokenRefreshDTO> {
+  const res = await fetch('/api/auth/refresh', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    credentials: 'include', // 쿠키 전송 (자동로그인 시)
+    body: JSON.stringify(data || {}),
+  });
+
+  const json = await res.json();
+
+  if (!res.ok) {
+    throw new Error(json.error?.message || 'Token refresh 실패');
+  }
+  return json;
+}
+
 //- ==================== 로그인 ====================
 export async function logIn(data: RequestLoginDTO): Promise<ResponseLoginDTO> {
   const res = await fetch('/api/auth/login', {
