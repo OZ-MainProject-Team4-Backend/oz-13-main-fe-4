b9e2a04a2a3748234f20968974cd0c0e0d5e6855
yeeun <jovm268@naver.com>
2025-11-11 10:40:14 +0900

    style : 반응형 사이즈 수정

--- src/styles/AuthStyle.tsx
+++ src/styles/AuthStyle.tsx
@@ -5,7 +5,7 @@ export const CardMui = styled(MuiCard)(({ theme }) => ({
   display: 'flex',
   flexDirection: 'column',
   alignSelf: 'center',
-  width: '100%',
+  // width: '100%',
   padding: theme.spacing(4),
   gap: theme.spacing(2),
   margin: 'auto',
