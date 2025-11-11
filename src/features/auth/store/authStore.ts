b8151ae1bd9b70da82f05e11c0f21da61960c870
yeeun <jovm268@naver.com>
2025-11-11 13:32:09 +0900

    chore : 필요없는 주석 및 코드 삭제

--- src/features/auth/store/authStore.ts
+++ src/features/auth/store/authStore.ts
@@ -5,7 +5,7 @@ import { Tokens, User } from '../types/auth';
 //- JWT token에서 액세스만 추가함(액세스와 자동로그인 처리만 전역메모리로 저장이 된다)
 type AuthState = {
   user: User | null; //유저의 타입 설정함
-  accessToken: string | null;
+  access: string | null;
   isAutoLogin: boolean; //자동로그인 분기처리 localStorage저장
   setAuth: (user: User, token: Tokens, isAutoLogin: boolean) => void; // 로그인 시 user, tokens, 자동로그인 여부 저장
   setAccessToken: (token: string) => void; //새로 발급받은 액세스토큰 업데이트
@@ -14,45 +14,36 @@ type AuthState = {
 };
 
 //- Zustand 스토어 (JWT 유저정보와 로그인여부만 로컬에 저장)
-/* persist 안에 accessToken을 넣은 건 Zustand 상태 관리용
-실제 localStorage 저장 여부는 partialize가 결정
-accessToken은 의도적으로 localStorage에서 제외됨 (보안)
-따라서 메모리에만 저장되는 게 맞다.
-동작 방식:
-전체 상태: user, accessToken, isAutoLogin
-localStorage 저장: user, isAutoLogin만 (partialize로 필터링)
-메모리에만: accessToken
-*/
 export const useAuthStore = create<AuthState>()(
   persist(
     (set, get) => ({
       //초기 전역 상태
       user: null,
-      accessToken: null,
+      access: null,
       isAutoLogin: false,
 
       //로그인시 호출
       setAuth: (user, tokens, isAutoLogin) =>
         set({
           user,
-          accessToken: tokens.accessToken, //새로고침하면 null초기화
+          access: tokens.access, //새로고침하면 null초기화
           isAutoLogin,
         }),
 
       //액세스 토큰 갱신 시 사용됨
       setAccessToken: (token) =>
         set({
-          accessToken: token,
+          access: token,
         }),
 
       // 리프레쉬토큰으로 새로운 액세스 토큰 발급 받기
-      getAccessToken: () => get().accessToken,
+      getAccessToken: () => get().access,
 
       //로그아웃시 토큰클리어
       clearAuth: () =>
         set({
           user: null,
-          accessToken: null,
+          access: null,
           isAutoLogin: false,
         }),
     }),
