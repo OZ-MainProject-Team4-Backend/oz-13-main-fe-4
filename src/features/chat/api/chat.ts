c391e11c7a0f515f95d425600e1310d9f35ca8d9
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-11-17 14:46:09 +0900

    chore: 엔드포인트 /api제거

--- src/features/chat/api/chat.ts
+++ src/features/chat/api/chat.ts
@@ -2,13 +2,13 @@ import { instance } from '../../../axios/instance';
 import { ReqSendMessage, ResSendMessage, ResChatHistory } from '../types/chat';
 
 export const sendMessage = async (message: ReqSendMessage): Promise<ResSendMessage> => {
-  const res = await instance.post('/api/chat/send', message);
+  const res = await instance.post('/chat/send', message);
 
   return res.data;
 };
 
 export const getChatHistory = async (): Promise<ResChatHistory> => {
-  const res = await instance.get('/api/chat/session');
+  const res = await instance.get('/chat/session');
 
   const messages = Array.isArray(res.data) ? res.data : [];
 
