b3381c6636fe5ce97cbef8547dc2450d6086420a
yeeun <jovm268@naver.com>
2025-11-17 17:18:54 +0900

    feat : ZOD 타입에서 비밀번호 비교 로직 추가

--- src/features/auth/types/zodTypes.ts
+++ src/features/auth/types/zodTypes.ts
@@ -8,7 +8,7 @@ export const signUpSchema = z
     name: z.string().min(1, '이름 입력은 필수입니다.'),
     nickname: z
       .string()
-      .min(1, '닉네임 입력 필수입니다.')
+      .min(2, '닉네임 입력 필수입니다.')
       .max(10, '닉네임은10자 이내로 작성해주세요'),
     gender: z.enum(['', 'M', 'F'], {
       message: '성별을 선택해주세요',
@@ -46,36 +46,50 @@ export const logInSchema = z.object({
 
 //- 마이페이지 스키마
 export const mypageSchema = z.object({
-  name: z.string().min(1, '이름 입력은 필수입니다.'),
+  name: z.string().min(1, '이름 입력은 필수입니다.').optional(),
   nickname: z
     .string()
-    .min(1, '닉네임 입력 필수입니다.')
-    .max(10, '닉네임은10자 이내로 작성해주세요'),
-  gender: z.enum(['', 'M', 'F'], {
-    message: '성별을 선택해주세요',
-  }),
-  age: z.enum(['', 'ten', 'twenty', 'thirty', 'fourthy', 'fifth', 'sixth'], {
-    message: '연령대를 선택해주세요',
-  }),
-  email: z.string().email('유효한 이메일 주소를 입력해주세요.'),
-  emailCode: z.string().min(6, '숫자코드6자리 입력해주세요').max(6, '숫자코드 6자리 입니다.'),
+    .min(2, '닉네임 입력 필수입니다.')
+    .max(10, '닉네임은10자 이내로 작성해주세요')
+    .optional(),
+  gender: z
+    .enum(['', 'M', 'F'], {
+      message: '성별을 선택해주세요',
+    })
+    .optional(),
+  age: z
+    .enum(['', 'ten', 'twenty', 'thirty', 'fourthy', 'fifth', 'sixth'], {
+      message: '연령대를 선택해주세요',
+    })
+    .optional(),
+  email: z.string().email('유효한 이메일 주소를 입력해주세요.').optional(),
+  emailCode: z
+    .string()
+    .min(6, '숫자코드6자리 입력해주세요')
+    .max(6, '숫자코드 6자리 입니다.')
+    .optional(),
 });
 
 export type FormFieldMypage = z.infer<typeof mypageSchema>;
 
 //- 비밀번호 스키마
-const passwordSchema = z
+export const passwordSchema = z
   .object({
-    oldPassword: z.string().min(1, '현재 비밀번호를 입력하세요'),
+    oldPassword: z.string().min(1, '현재 비밀번호를 입력하세요'), // ✅ 유지
     newPassword: z
       .string()
-      .min(6, '6자 이상')
-      .regex(/^(?=.*[a-z])(?=.*[0-9])/, '영문+숫자 조합'),
+      .min(6, '비밀번호는 6자 이상 입력해주세요')
+      .max(20, '비밀번호는 20자 이하로 입력해주세요')
+      .regex(/^(?=.*[a-z])(?=.*[0-9])[a-z0-9]+$/, '영문 소문자와 숫자 조합으로 입력해주세요'),
     newPasswordConfirm: z.string(),
   })
   .refine((data) => data.newPassword === data.newPasswordConfirm, {
     message: '새 비밀번호가 일치하지 않습니다',
     path: ['newPasswordConfirm'],
+  })
+  .refine((data) => data.oldPassword !== data.newPassword, {
+    message: '새 비밀번호는 현재 비밀번호와 달라야 합니다.',
+    path: ['newPassword'], // 👈 에러 위치
   });
 
 export type FormFieldPassword = z.infer<typeof passwordSchema>;
