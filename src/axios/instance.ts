68370bc85ba55540240b7a68974a7fb48609c022
yeeun <jovm268@naver.com>
2025-11-18 00:12:03 +0900

    api : 서버 api주소로 변경

--- src/axios/instance.ts
+++ src/axios/instance.ts
@@ -1,12 +1,15 @@
 import axios from 'axios';
 export const instance = axios.create({
-  baseURL: 'http://localhost:5173/api',
+  baseURL: 'https://www.team4.p-e.kr/api', //env 환경변수로 설정하기
   timeout: 1000,
   headers: { 'Content-Type': 'application/json' },
   withCredentials: true, //리프레쉬토큰의 쿠키 송수신 허용
 });
 
 export const diaryInstance = axios.create({
-  baseURL: 'http://localhost:5173/api',
+  baseURL: 'https://www.team4.p-e.kr/api',
   timeout: 1000,
 });
+
+//로그인시 유효성 검사 메세지는 숨겨
+//패스워드 확인도 같이 보내나ㅏ?
