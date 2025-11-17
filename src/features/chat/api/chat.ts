ee34784267b80b57527dfd2e261d25eff6cad00a
Dabin Kwon <ekqlsekqls8857@gmail.com>
2025-11-17 16:33:26 +0900

    feat: getChatHistory 함수 수정 및 getChatLogs 함수 구현

--- src/features/chat/api/chat.ts
+++ src/features/chat/api/chat.ts
@@ -1,5 +1,5 @@
 import { instance } from '../../../axios/instance';
-import { ReqSendMessage, ResSendMessage, ResChatHistory } from '../types/chat';
+import { ReqSendMessage, ResSendMessage, ResChatHistory, ResChatLogs } from '../types/chat';
 
 export const sendMessage = async (message: ReqSendMessage): Promise<ResSendMessage> => {
   const res = await instance.post('/chat/send', message);
@@ -10,9 +10,23 @@ export const sendMessage = async (message: ReqSendMessage): Promise<ResSendMessa
 export const getChatHistory = async (): Promise<ResChatHistory> => {
   const res = await instance.get('/chat/session');
 
-  const messages = Array.isArray(res.data) ? res.data : [];
+  const messages = res.data?.messages && Array.isArray(res.data.messages) ? res.data.messages : [];
 
   return {
+    session_id: res.data?.session_id || '',
+    created_at: res.data?.created_at || '',
     messages,
+    next_before_id: res.data?.next_before_id ?? null,
+    has_more: res.data?.has_more || false,
+  };
+};
+
+export const getChatLogs = async (): Promise<ResChatLogs> => {
+  const res = await instance.get('/chat/logs');
+
+  const logs = Array.isArray(res.data) ? res.data : [];
+
+  return {
+    logs,
   };
 };
