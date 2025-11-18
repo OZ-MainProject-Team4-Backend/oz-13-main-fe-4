a5844e8032cb8d47d4c57e8c7d065881baafa05d
leesm <ask135799@naver.com>
2025-11-18 23:34:57 +0900

    fix: 의상추천 api request 수정 (get > post)

--- src/features/recommendation/api/clothingAPI.ts
+++ src/features/recommendation/api/clothingAPI.ts
@@ -10,16 +10,16 @@ export interface OutfitRecommendation {
 export const outfitAPI = {
   // 좌표 기반 의상 추천 (현재 날씨용) 1118 현재 500 에러 발생, query string이 존재하지않음 확인필요
   getRecommendationByCoords: async (lat: number, lon: number) => {
-    const response = await instance.get<OutfitRecommendation>(
-      // `/recommend/outfit?lat=${lat}&lon=${lon}`
-      `/recommend/coords/`
+    const response = await instance.post<OutfitRecommendation>(
+      `/recommend/outfit?lat=${lat}&lon=${lon}`
+      // `/recommend/coords/`
     );
     return response.data;
   },
 
   // 주소 기반 의상 추천 (즐겨찾기 지역용) 1118 현재 500에러 발생 위와 같음
   getRecommendationByLocation: async (location: string) => {
-    const response = await instance.get<OutfitRecommendation>(`/recommend/location/`);
+    const response = await instance.post<OutfitRecommendation>(`/recommend/location/`);
     return response.data;
   },
 };
