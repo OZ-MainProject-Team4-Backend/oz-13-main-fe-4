db3b4504f6f15606243edd0b5c1eb10c65cbcb4a
leesm <ask135799@naver.com>
2025-11-18 23:25:03 +0900

    chore: 이전 커밋 누락된 내용 추가

--- src/features/recommendation/api/clothingAPI.ts
+++ src/features/recommendation/api/clothingAPI.ts
@@ -8,19 +8,18 @@ export interface OutfitRecommendation {
 }
 
 export const outfitAPI = {
-  // 좌표 기반 의상 추천 (현재 날씨용) 1118 현재 500 에러 발생
+  // 좌표 기반 의상 추천 (현재 날씨용) 1118 현재 500 에러 발생, query string이 존재하지않음 확인필요
   getRecommendationByCoords: async (lat: number, lon: number) => {
     const response = await instance.get<OutfitRecommendation>(
-      `/recommend/outfit?lat=${lat}&lon=${lon}`
+      // `/recommend/outfit?lat=${lat}&lon=${lon}`
+      `/recommend/coords/`
     );
     return response.data;
   },
 
-  // 주소 기반 의상 추천 (즐겨찾기 지역용) 1118 현재 500에러 발생
+  // 주소 기반 의상 추천 (즐겨찾기 지역용) 1118 현재 500에러 발생 위와 같음
   getRecommendationByLocation: async (location: string) => {
-    const response = await instance.get<OutfitRecommendation>(
-      `/recommend/outfit?location=${location}`
-    );
+    const response = await instance.get<OutfitRecommendation>(`/recommend/location/`);
     return response.data;
   },
 };
